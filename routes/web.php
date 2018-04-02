7badb642c532528030b79adc00ad49e8904403a0
geneowak <teugeneowak@gmail.com>
2018-04-03 00:19:21 +0300

    did some tests for node status configurations

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::get('/configuresensor', function () {
     return view('layouts/configuresensor');
 });
 
-Route::get('/faker',function(){
+Route::get('/tester',function(){
 
     // "date_time_recorded": "2018-02-28 15:40:19"
     // $tasks = DB::table('observationslip')->where('CreationDate','>=','2018-01-01 00:00:00')
@@ -59,21 +59,24 @@ Route::get('/faker',function(){
     $date = new DateTime("now", $zone);// get current time
     // $date->sub(new DateInterval('PT1H'));// subtract one hour from current time
     $date = $date->format('Y-m-d H:m:s');// change date time object to string format used in the database.
+
     // test variables
-    $vinNull = 0;
-    $vmcuNull = 0;
-    $dateNull = 0;
-    $vinMin = 0;
-    $vinMax = 0;
-    $vmcuMin = 0;
-    $vmcuMax = 0;
-    $dateMin = 0;
-    $dateMax = 0;
-    $date1970 = 0;
-    $dateArray = array();
+    $vinNull = 0; $vmcuNull = 0; $dateNull = 0; $vinMin = 0; $vinMax = 0; $vmcuMin = 0; $vmcuMax = 0; $dateMin = 0; $dateMax = 0; $date1970 = 0; $invalidDates = array();
+
+    // pick configuration info
+    $nodeConf = DB::table('node_status_configurations')->select('txt_value','v_in_min_value','v_in_max_value','v_mcu_min_value','v_mcu_max_value')->get();
+
     // pick only columns that we'll be using. We won't need date_time_recorded because we have
-    $counts = DB::table('nodestatus')->select('V_MCU','V_IN','date','time','TXT','StationNumber')->orderBy('id')->chunk(100, function($nodes) use(&$vinNull, &$vmcuNull, &$dateNull, &$vinMin, &$vinMax, &$vmcuMin, &$vmcuMax, &$dateMin, &$dateMax, &$date, &$date1970, &$dateArray){
+    $counts = DB::table('nodestatus')->select('V_MCU','V_IN','date','time','TXT','StationNumber')->orderBy('id')->chunk(100, function($nodes) use(&$vinNull, &$vmcuNull, &$dateNull, &$vinMin, &$vinMax, &$vmcuMin, &$vmcuMax, &$dateMin, &$dateMax, &$date, &$date1970, &$invalidDates, &$nodeConf){
         foreach ($nodes as $node) {
+            foreach ($nodeConf as $nodeConfig) {
+                if ($nodeConfig->txt_value === $node->TXT) {// if the txt matches then get the config info
+                    $vinMaxVal = $nodeConfig->v_in_max_value;
+                    $vinMinVal = $nodeConfig->v_in_min_value;
+                    $vmcuMaxVal = $nodeConfig->v_mcu_max_value;
+                    $vmcuMinVal = $nodeConfig->v_mcu_min_value;
+                }
+            }
             //check for nulls
             if ($node->V_MCU == ''| $node->V_MCU == null) {
                 $vmcuNull++;
@@ -85,40 +88,38 @@ Route::get('/faker',function(){
                 $dateNull++;
             }    
             // check for mins        
-            if ($node->V_MCU < '3.5') {
+            if ($node->V_MCU < $vmcuMinVal) {
                 $vinMin++;
             }
-            if ($node->V_IN < '3.5') {
+            if ($node->V_IN < $vinMinVal) {
                 $vmcuMin++;
             }
             $yearNow = substr($date,0,4);
             $yearRec = substr($node->date,0,4);
             if ($yearRec < $yearNow) {
                 $dateMin++;
-                $dateArray[] = $yearRec;
+                $invalidDates[] = $yearRec;
             }            
             // check for maxs        
-            if ($node->V_MCU > '3.5') {
+            if ($node->V_MCU > $vmcuMaxVal) {
                 $vinMax++;
             }
-            if ($node->V_IN > '3.5') {
+            if ($node->V_IN > $vinMaxVal) {
                 $vmcuMax++;
             }
-            $yearNow = substr($date,0,4);
-            $yearRec = substr($node->date,0,4);
             if ($yearRec > $yearNow) {
                 $dateMax++;
-                $dateArray[] = $yearRec;
+                $invalidDates[] = $yearRec;
             }            
             if ($yearRec == '1970') {
                 $date1970++;
             }            
         }
     });
-    $wrongDateString = "";
-    $wrongDates = array_count_values($dateArray);
+    $wrongDateString = "\n";
+    $wrongDates = array_count_values($invalidDates);
     foreach ($wrongDates as $key => $value) {
-        $wrongDateString .= $key." appears ". $value." times. \n";
+        $wrongDateString .= $key." appears ". $value." time(s). \n";
     };
     $string = "V_IN nulls: ".$vinNull."\n"."V_MCU nulls: ".$vmcuNull."\n"."Date nulls: ".$dateNull."\n"."V_IN mins: ".$vinMin."\n"."V_IN max: ".$vinMax."\n"."V_MCU mins: ".$vmcuMin."\n"."V_MCU max: ".$vmcuMax."\n"."V_IN nulls: ".$dateMin."\n"."Date Max: ".$dateMax."\n"."1970 Date: ".$date1970."\n"."Wrong Dates: ".$wrongDateString;
 
@@ -126,5 +127,5 @@ Route::get('/faker',function(){
     dd($string);
 
     // return $datas;
-    return view('layouts/fakerTests', compact('datas'));
+    return view('layouts/tester', compact('datas'));
 });
