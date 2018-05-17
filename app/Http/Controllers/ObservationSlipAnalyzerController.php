7aae30bf6f11bc71d804b6f075c53593f3c3a31d
geneowak <teugeneowak@gmail.com>
2018-05-17 14:47:12 +0300

    added AnalyzerDBHander and ObservationSlipAnalyzerController

---
+++ app/Http/Controllers/ObservationSlipAnalyzerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace station\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ObservationSlipAnalyzerController extends Controller
+{
+    public function __construct(Type $var = null)
+    {
+        # code...
+    }
+}
